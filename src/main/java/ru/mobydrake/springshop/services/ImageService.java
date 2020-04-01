e88dd3b9a8e196d94e04cf6a379310704dde3b64
MobyDrake <Milkky598@yandex.ru>
2020-04-01 14:26:43 +0500

    change load images and icon

--- src/main/java/ru/mobydrake/springshop/services/ImageService.java
+++ src/main/java/ru/mobydrake/springshop/services/ImageService.java
@@ -3,10 +3,13 @@ package ru.mobydrake.springshop.services;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import ru.mobydrake.springshop.persistence.repositories.ImageRepository;
+import ru.mobydrake.springshop.utils.Validators;
 
 
 import javax.imageio.ImageIO;
@@ -18,12 +21,18 @@ import java.io.IOException;
 
 import java.nio.charset.MalformedInputException;
 
+import java.nio.file.Path;
 import java.util.UUID;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ImageService {
+
+    @Value("${files.storepath.images}")
+    private Path IMAGES_STORE_PATH;
+    @Value("${files.storepath.icons}")
+    private Path ICONS_STORE_PATH;
 
     private final ImageRepository imageRepository;
 
@@ -32,16 +41,44 @@ public class ImageService {
     }
 
     public BufferedImage loadFileAsResource(String id) throws IOException {
+//        try {
+//            String imageName = getImageForSpecificProduct(UUID.fromString(id));
+//            Resource resource = new ClassPathResource("/static/images/" + imageName);
+//            if (resource.exists()) {
+//                return ImageIO.read(resource.getFile());
+//            } else {
+//                log.error("Image not found!");
+//                throw new FileNotFoundException("File " + imageName + " not found!");
+//            }
+//        } catch (MalformedInputException | FileNotFoundException ex) {
+//            return null;
+//        }
+        String imageName = null;
+
         try {
-            String imageName = getImageForSpecificProduct(UUID.fromString(id));
-            Resource resource = new ClassPathResource("/static/images/" + imageName);
-            if (resource.exists()) {
-                return ImageIO.read(resource.getFile());
+            Path filePath;
+
+            if (Validators.isUUID(id)) {
+                imageName = getImageForSpecificProduct(UUID.fromString(id));
+
+                if (imageName != null) {
+                    filePath = IMAGES_STORE_PATH.resolve(imageName).normalize();
+                } else {
+                    imageName = "image_not_found.png";
+                    filePath = ICONS_STORE_PATH.resolve(imageName).normalize();
+                }
             } else {
-                log.error("Image not found!");
-                throw new FileNotFoundException("File " + imageName + " not found!");
+                filePath = ICONS_STORE_PATH.resolve(id).normalize();
             }
-        } catch (MalformedInputException | FileNotFoundException ex) {
+
+            if (filePath != null) {
+                return ImageIO.read(new UrlResource(filePath.toUri()).getFile());
+            } else {
+                throw new IOException();
+            }
+
+        } catch (IOException ex) {
+            log.error("Error! Image {} file wasn't found!", imageName);
             return null;
         }
     }
